3c81ec0966742e347f5147d926d89f8727d8bf4a
Ricardo Canales <canalesb93@gmail.com>
2015-11-11 20:51:08 -0600

    Initial Commit

---
+++ Mantle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mantle
+//
+//  Created by Ricardo Canales on 11/11/15.
+//  Copyright © 2015 canalesb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
